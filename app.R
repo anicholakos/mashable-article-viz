54271b8792c5d711996f0825a18e15dcc1133c70
anicholakos <anicholakos@gmail.com>
2019-09-14 17:00:41 -0400

    Clean up dataset
    - Added extra columns to dataset to manipulate and read easier.
    Merged different weekday columns into one with 7 different values.
    Merged different data channel columns into one with 6 different values.

--- app.R
+++ app.R
@@ -1,7 +1,47 @@
 library(shiny)
 library(DT)
 library(ggplot2)
+library(tidyverse)
+
+# Add column values to better organize data
 mashableData <- read.csv(file="OnlineNewsPopularity.csv", header=TRUE, sep=",")
+mashableData <- mashableData %>%
+  mutate(weekend = if_else(is_weekend == 0, "False", 
+                           ifelse(is_weekend == 1, "True", NA)))
+
+mashableData <- mashableData %>% 
+  mutate(weekday = if_else(weekday_is_monday == 1, "Monday",
+                           ifelse(weekday_is_tuesday == 1, "Tuesday", 
+                                  ifelse(weekday_is_wednesday == 1, "Wednesday", 
+                                         ifelse(weekday_is_thursday == 1, "Thursday", 
+                                                ifelse(weekday_is_friday == 1, "Friday", 
+                                                       ifelse(weekday_is_saturday == 1, "Saturday", 
+                                                              ifelse(weekday_is_sunday == 1, "Sunday", NA))))))))
+
+mashableData <- mashableData %>% 
+  mutate(weekday_value = if_else(weekday_is_monday == 1, 1,
+                                 ifelse(weekday_is_tuesday == 1, 2, 
+                                        ifelse(weekday_is_wednesday == 1, 3, 
+                                               ifelse(weekday_is_thursday == 1, 4, 
+                                                      ifelse(weekday_is_friday == 1, 5, 
+                                                             ifelse(weekday_is_saturday == 1, 6, 
+                                                                    ifelse(weekday_is_sunday == 1, 7, NA))))))))
+
+mashableData <- mashableData %>% 
+  mutate(data_channel = ifelse(data_channel_is_lifestyle == 1, "Lifestyle",
+                               ifelse(data_channel_is_entertainment == 1, "Entertainment", 
+                                      ifelse(data_channel_is_socmed == 1, "Social Media", 
+                                             ifelse(data_channel_is_bus == 1, "Business",
+                                                    ifelse(data_channel_is_tech == 1, "Tech",
+                                                           ifelse(data_channel_is_world == 1, "World", NA)))))))
+
+mashableData <- mashableData %>% 
+  mutate(data_channel_value = ifelse(data_channel_is_lifestyle == 1, 1,
+                                     ifelse(data_channel_is_entertainment == 1, 2, 
+                                            ifelse(data_channel_is_socmed == 1, 3, 
+                                                   ifelse(data_channel_is_bus == 1, 4,
+                                                          ifelse(data_channel_is_tech == 1, 5,
+                                                                 ifelse(data_channel_is_world == 1, 6, NA)))))))
 
 # Initialize the UI
 ui <- fluidPage(
